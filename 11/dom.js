b346223b404d3a15a0badbde3c67487a88abe139
Sasha984 <aleksandratorgan34@gmail.com>
2024-04-13 17:17:56 +0300

    11

---
+++ 11/dom.js
@@ -0,0 +1,71 @@
+window.onload = function(){
+    //Завдання 1: Конвертер валют
+    //Мета: Створити простий конвертер валют, який перетворює введені користувачем гроші з однієї валюти в іншу.
+    
+    let myForm = document.querySelector('form');
+    let uah = document.querySelector('#uah');
+    let usd = document.querySelector('#usd');
+    let change = document.querySelector('#change');
+    let pUAH = document.querySelector('#inuah');
+    let pUSD = document.querySelector('#inusd');
+
+    myForm.addEventListener('submit', function(event){  
+    event.preventDefault();
+    let uahValue = event.target['uah'].value;
+    let usdValue = event.target['usd'].value;
+    if(uahValue!=0 && usdValue.length ==0 && uahValue>0 ){
+        pUAH.innerHTML= 'Это будет'+uahValue/40 +'USD';
+        pUAH.style.display = 'inherit';
+        pUSD.style.display = 'none';
+    }
+    else if (usdValue!=0 && uahValue.length ==0 && usdValue>0){
+        pUSD.innerHTML= 'Это будет '+usdValue*40 +'UAH';
+        pUSD.style.display = 'inherit';
+        pUAH.style.display = 'none';
+    }
+    else{
+        alert('Введите или USD, или UAH');
+    }
+    });
+
+
+
+
+
+
+
+
+
+// 2) Створіть слайдер ( зображення що міняє картинку ) кожні 3 секунди,  коли курсор наведено на зображення слайдер зупиняється, коли покидає зображення розпочинає роботу
+
+
+    let images = ['./img/lion.jpg','./img/lion2.jpg','./img/lion3.jpg','./img/lion4.jpg'];
+    
+    let Img = document.querySelector('img');
+
+    let curentIndex = 0;
+
+    function newPic(){
+        curentIndex += 1;
+        if(curentIndex >= images.length){
+            curentIndex = 0;
+        }
+                        
+        document.querySelector('img').src = images[curentIndex];
+    }
+
+    let slider = setInterval(newPic, 2000); 
+
+    Img.addEventListener('mouseleave', function(){
+        slider = setInterval(newPic, 2000);
+
+    });
+
+    Img.addEventListener('mouseenter', function(){
+        clearInterval(slider);
+
+    });
+    
+
+
+}
